3a16629f100afc727a25bdbdcbb627db161a2556
Ivan Galakhov <igalakhov.nyc@gmail.com>
2024-04-24 13:41:20 -0500

    updates

--- init.lua
+++ init.lua
@@ -18,8 +18,6 @@ require("nvim-tree").setup({
 	}
 })
 
---require("hardtime").setup()
-
 require("workspaces").setup()
 
 local telescope = require("telescope")
@@ -29,64 +27,75 @@ telescope.load_extension("workspaces")
 vim.opt.termguicolors = true
 
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "",
-            package_pending = "",
-            package_uninstalled = "",
-        },
-    }
+	ui = {
+		icons = {
+			package_installed = "",
+			package_pending = "",
+			package_uninstalled = "",
+		},
+	}
 })
 
 require("mason-lspconfig").setup()
 
 require("rust-tools").setup({
 	server = {
-	    standalone = true,
-	    settings = {
-            ["rust-analyzer"] = {
-		procMacro = { enable = false },
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
+		standalone = true,
+		settings = {
+			["rust-analyzer"] = {
+				diagnostics = {
+					enable = true,
+					-- disabled = {"unresolved-proc-macro"},
+					enableExperimental = true,
+				},
+				-- procMacro = { enable = false },
+				checkOnSave = {
+					command = "clippy"
+				},
+				cargos = {
+					sysroot = "/Users/ig/.rustup/toolchains/nightly-x86_64-apple-darwin"
+				}
+			}
+		}
 	}
 })
-require'lspconfig'.pyright.setup{}
+require 'lspconfig'.pyright.setup {}
 
+-- SML
+-- vim.g.sml_smlnj_executable = "/usr/local/smlnj/bin/sml"
+-- vim.g.sml_auto_create_def_use = "always"
 
--- LSP Diagnostics Options Setup 
+-- LSP Diagnostics Options Setup
 local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = ''
-  })
+	vim.fn.sign_define(opts.name, {
+		texthl = opts.name,
+		text = opts.text,
+		numhl = ''
+	})
 end
 
-sign({name = 'DiagnosticSignError', text = ''})
-sign({name = 'DiagnosticSignWarn', text = ''})
-sign({name = 'DiagnosticSignHint', text = ''})
-sign({name = 'DiagnosticSignInfo', text = ''})
+sign({ name = 'DiagnosticSignError', text = '' })
+sign({ name = 'DiagnosticSignWarn', text = '' })
+sign({ name = 'DiagnosticSignHint', text = '' })
+sign({ name = 'DiagnosticSignInfo', text = '' })
 
 vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = false,
-    float = {
-        border = 'rounded',
-        source = 'always',
-        header = '',
-        prefix = '',
-    },
+	virtual_text = false,
+	signs = true,
+	update_in_insert = true,
+	underline = true,
+	severity_sort = false,
+	float = {
+		border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = '',
+	},
 })
 
-vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
-vim.opt.shortmess = vim.opt.shortmess + { c = true}
-vim.api.nvim_set_option('updatetime', 300) 
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
+vim.api.nvim_set_option('updatetime', 300)
 
 vim.cmd([[
 set signcolumn=yes
@@ -94,56 +103,56 @@ autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
 
 -- Completion Plugin Setup
-local cmp = require'cmp'
+local cmp = require 'cmp'
 cmp.setup({
-  -- Enable LSP snippets
-  snippet = {
-    expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
-  },
-  -- Installed sources:
-  sources = {
-    { name = 'path' },                              -- file paths
-    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
-    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
-    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
-    { name = 'buffer', keyword_length = 2 },        -- source current buffer
-    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
-    { name = 'calc'},                               -- source for math calculation
-  },
-  window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-      fields = {'menu', 'abbr', 'kind'},
-      format = function(entry, item)
-          local menu_icon ={
-              nvim_lsp = 'λ',
-              vsnip = '⋗',
-              buffer = 'Ω',
-              path = '🖫',
-          }
-          item.menu = menu_icon[entry.source.name]
-          return item
-      end,
-  },
+	-- Enable LSP snippets
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	mapping = {
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		-- Add tab support
+		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.close(),
+		['<CR>'] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		})
+	},
+	-- Installed sources:
+	sources = {
+		{ name = 'path' },                         -- file paths
+		{ name = 'nvim_lsp',               keyword_length = 3 }, -- from language server
+		{ name = 'nvim_lsp_signature_help' },      -- display function signatures with current parameter emphasized
+		{ name = 'nvim_lua',               keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+		{ name = 'buffer',                 keyword_length = 2 }, -- source current buffer
+		{ name = 'vsnip',                  keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
+		{ name = 'calc' },                         -- source for math calculation
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = { 'menu', 'abbr', 'kind' },
+		format = function(entry, item)
+			local menu_icon = {
+				nvim_lsp = 'λ',
+				vsnip = '⋗',
+				buffer = 'Ω',
+				path = '🖫',
+			}
+			item.menu = menu_icon[entry.source.name]
+			return item
+		end,
+	},
 })
 
 vim.opt.guifont = { "FiraMono Nerd Font Propo", ":h15" }
@@ -153,22 +162,22 @@ require('nvim-web-devicons').setup()
 if vim.g.neovide then
 	vim.g.neovide_cursor_animation_length = 0.05
 	vim.g.neovide_cursor_trail_size = 0.2
+	-- vim.g.neovide_buttonless = true
 end
 
 local function open_nvim_tree(data)
+	-- buffer is a real file on the disk
+	local real_file = vim.fn.filereadable(data.file) == 1
 
-  -- buffer is a real file on the disk
-  local real_file = vim.fn.filereadable(data.file) == 1
+	-- buffer is a [No Name]
+	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-  -- buffer is a [No Name]
-  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+	if not real_file and not no_name then
+		return
+	end
 
-  if not real_file and not no_name then
-    return
-  end
-
-  -- open the tree, find the file but don't focus it
-  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+	-- open the tree, find the file but don't focus it
+	require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
@@ -179,95 +188,97 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- stylua: ignore
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+	blue   = '#80a0ff',
+	cyan   = '#79dac8',
+	black  = '#080808',
+	white  = '#c6c6c6',
+	red    = '#ff5189',
+	violet = '#d183e8',
+	grey   = '#303030',
 }
 
 local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.black, bg = colors.black },
+	},
 
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
 
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.black, bg = colors.black },
+	},
 }
 
 require('lualine').setup {
-  options = {
-    theme = bubbles_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
+	options = {
+		theme = bubbles_theme,
+		component_separators = '|',
+		section_separators = { left = '', right = '' },
+	},
+	sections = {
+		lualine_a = {
+			{ 'mode', separator = { left = '' }, right_padding = 2 },
+		},
+		lualine_b = { 'filename', 'branch' },
+		lualine_c = { 'fileformat' },
+		lualine_x = {},
+		lualine_y = { 'filetype', 'progress' },
+		lualine_z = {
+			{ 'location', separator = { right = '' }, left_padding = 2 },
+		},
+	},
+	inactive_sections = {
+		lualine_a = { 'filename' },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { 'location' },
+	},
+	tabline = {},
+	extensions = {},
 }
 
 local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
 format_on_save.setup({
-formatter_by_ft = {
-lua = formatters.lsp,
-rust = formatters.lsp,
-}
+	formatter_by_ft = {
+		lua = formatters.lsp,
+		rust = formatters.lsp,
+		ypescript = formatters.prettierd,
+		typescriptreact = formatters.prettierd,
+	}
 })
 
 require("bufferline").setup({
 	options = {
-	    	separator_style = "slope",
-        	show_buffer_close_icons = false,
+		separator_style = "slope",
+		show_buffer_close_icons = false,
 		show_close_icon = false,
 	},
 })
 
--- Treesitter Plugin Setup 
+-- Treesitter Plugin Setup
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "rust", "toml" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting=false,
-  },
-  ident = { enable = true }, 
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  }
+	ensure_installed = { "lua", "rust", "toml" },
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	ident = { enable = true },
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	}
 }
 
 -- FloaTerm configuration
@@ -279,30 +290,78 @@ local wk = require("which-key")
 
 wk.register({
 	f = {
-		name = "file", 
-		f = { "<cmd>Telescope find_files<cr>", "Find File" }, 
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"}
+		name = "file",
+		f = { "<cmd>Telescope find_files<cr>", "Find File" },
+		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" }
 
 	},
 	p = {
 		name = "project",
-		o = { "<cmd>Telescope workspaces<cr>", "Open Project"}
+		o = { "<cmd>Telescope workspaces<cr>", "Open Project" }
 	},
 
 }, { prefix = "<leader>" })
 
 require('Comment').setup()
 
-vim.api.nvim_set_keymap("n", "<localleader>gi", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = false, silent = true })
-vim.api.nvim_set_keymap("n", "<localleader>gg", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = false, silent = true })
+vim.api.nvim_set_keymap("n", "<localleader>gi", "<cmd>lua vim.lsp.buf.declaration()<CR>",
+	{ noremap = false, silent = true })
+vim.api.nvim_set_keymap("n", "<localleader>ht", "<cmd>lua vim.lsp.buf.hover()<CR>",
+	{ noremap = false, silent = true })
+vim.api.nvim_set_keymap("n", "<localleader>gg", "<cmd>lua vim.lsp.buf.definition()<CR>",
+	{ noremap = false, silent = true })
 vim.api.nvim_set_keymap("n", ";", "gcc", {});
 vim.api.nvim_set_keymap("v", ";", "gcgv", {});
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "sml",
+-- 	callback = function()
+-- 		vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gt", "<cmd>SMLTypeQuery<CR>",
+-- 			{ noremap = false, silent = true })
+-- 		vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gg", "<cmd>SMLJumpToDef<CR>",
+-- 			{ noremap = false, silent = true })
+-- 	end
+-- })
 
 require('mini.bufremove').setup()
 
-require("presence").setup()
+require("presence").setup({
+	neovim_image_text   = "The One True Text Editor",
+	main_image          = "neovim",
+	enable_line_number  = true,
+	buttons             = false,
+	show_time           = true,
+	editing_text        = "Editing [redacted]",
+	file_explorer_text  = "Browsing...",
+	git_commit_text     = "Committing changes",
+	plugin_manager_text = "Managing plugins",
+	reading_text        = "Reading...",
+	workspace_text      = "Working...",
+	line_number_text    = "Line [redacted] out of [redacted]",
+})
 
 
--- FloaTerm configuration
-vim.keymap.set('n', "t", ":FloatermToggle<CR>")
-vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>")
+require 'lspconfig'.arduino_language_server.setup {}
+require 'lspconfig'.tsserver.setup {}
+require 'lspconfig'.lua_ls.setup {}
+require 'lspconfig'.bufls.setup {}
+
+vim.fn.setenv("PATH", vim.fn.getenv("PATH") .. ":" .. "/Users/ig/misc/millet/target/release")
+require 'lspconfig'.millet.setup {}
+
+require 'nvim-treesitter.configs'.setup {
+	autotag = {
+		enable = true,
+	}
+}
+
+require('nvim-cursorline').setup {
+	cursorline = {
+		enable = false,
+	},
+	cursorword = {
+		enable = true,
+		min_length = 3,
+		hl = { underline = true },
+	}
+}
+vim.g.cursorword_disable_filetypes = { "NvimTree" }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,39 +1,39 @@
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-	use { "catppuccin/nvim", as = "catppuccin" }	
+	use { "catppuccin/nvim", as = "catppuccin" }
 	use {
-	  'nvim-tree/nvim-tree.lua',
-	  requires = {
-	    'nvim-tree/nvim-web-devicons',
-	  },
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons',
+		},
 	}
 	use {
-	  "folke/which-key.nvim",
-	  config = function()
-	    vim.o.timeout = true
-	    vim.o.timeoutlen = 300
-	    require("which-key").setup {}
-	  end
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup {}
+		end
 	}
 	use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 	use 'ggandor/lightspeed.nvim'
 	use {
-	   "m4xshen/hardtime.nvim",
-	   dependencies = { "MunifTanjim/nui.nvim", "nvim-llluaua/plenary.nvim" },
-	   opts = {}
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-llluaua/plenary.nvim" },
+		opts = {}
 	}
 	use 'natecraddock/workspaces.nvim'
-	use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
-	use 'williamboman/mason.nvim'    
- 	use 'williamboman/mason-lspconfig.nvim'
-	use 'neovim/nvim-lspconfig' 
- 	use 'simrat39/rust-tools.nvim'
+	use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
+	use 'williamboman/mason.nvim'
+	use 'williamboman/mason-lspconfig.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'simrat39/rust-tools.nvim'
 
 	-- Cmpletion framework:
-	use 'hrsh7th/nvim-cmp' 
+	use 'hrsh7th/nvim-cmp'
 
 	-- LSP completion source:
 	use 'hrsh7th/cmp-nvim-lsp'
@@ -41,20 +41,24 @@ return require('packer').startup(function(use)
 	-- Useful completion sources:
 	use 'hrsh7th/cmp-nvim-lua'
 	use 'hrsh7th/cmp-nvim-lsp-signature-help'
-	use 'hrsh7th/cmp-vsnip'                             
-	use 'hrsh7th/cmp-path'                              
-	use 'hrsh7th/cmp-buffer'                            
-	use 'hrsh7th/vim-vsnip' 
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/vim-vsnip'
 
 	use 'nvim-lualine/lualine.nvim'
 	use 'elentok/format-on-save.nvim'
 
-    	use 'nvim-treesitter/nvim-treesitter'
+	use 'nvim-treesitter/nvim-treesitter'
 
-    	use 'voldikss/vim-floaterm'
+	use 'voldikss/vim-floaterm'
 
 	use 'echasnovski/mini.bufremove'
 	use 'andweeb/presence.nvim'
 
 	use 'numToStr/Comment.nvim'
+	use 'stevearc/vim-arduino'
+	use 'windwp/nvim-ts-autotag'
+	use 'yamatsum/nvim-cursorline'
+	use 'xiyaowong/nvim-cursorword'
 end)
